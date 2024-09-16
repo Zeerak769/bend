8a772dea8ef5fc731300070b4ef595c1fa372793
Zeerak769 <zeeraknoumansiddiqui000@gmail.com>
2024-09-16 18:04:51 +0500

    Initial commit

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
@@ -6,7 +5,7 @@ const cors = require('cors');
 const materialRoutes = require('./routes/materialRoutes');
 const requestRoutes = require('./routes/requestRoutes');
 const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes'); // Import userRoutes
+const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 connectDB();
@@ -14,19 +13,18 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin: '*' // Allow requests from this origin
+    origin: '*' // Adjust as needed
 }));
 
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/requests', requestRoutes);
 app.use('/api/materials', materialRoutes);
-app.use('/api/users', userRoutes); // Add userRoutes here
+app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/', (req, res) => {
+    res.send('Backend is running');
+});
 
 module.exports = app;
